afae41068a5fabea8f06675018b734f44d251229
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-31 05:01:26 +0300

    first commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+==========
+*pyapikey*
+==========
+
+.. image:: https://img.shields.io/pypi/v/pyapikey
+
+.. image:: https://img.shields.io/github/license/veltzer/pyapikey
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+
+project website: https://veltzer.github.io/pyapikey
+
+author: Mark Veltzer
+
+version: 0.0.1
+
+
---
+++ setup.py
@@ -0,0 +1,58 @@
+import setuptools
+
+
+def get_readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setuptools.setup(
+    # the first three fields are a must according to the documentation
+    name="pyapikey",
+    version="0.0.1",
+    packages=[
+        'pyapikey',
+    ],
+    # from here all is optional
+    description="access api keys from code",
+    long_description=get_readme(),
+    long_description_content_type="text/x-rst",
+    author="Mark Veltzer",
+    author_email="mark.veltzer@gmail.com",
+    maintainer="Mark Veltzer",
+    maintainer_email="mark.veltzer@gmail.com",
+    keywords=[
+        'api',
+        'key',
+        'python',
+        'secret',
+    ],
+    url="https://veltzer.github.io/pyapikey",
+    download_url="https://github.com/veltzer/pyapikey",
+    license="MIT",
+    platforms=[
+        'python3',
+    ],
+    install_requires=[
+    ],
+    extras_require={
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+    data_files=[
+    ],
+    entry_points={"console_scripts": [
+    ]},
+    python_requires=">=3.6",
+)
