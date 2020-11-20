7f138a0188c1297a9246907fa4d9bff3978dca90
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-20 12:37:19 +0200

    dependencies do not need explanations

--- config/python.py
+++ config/python.py
@@ -19,14 +19,13 @@ install_requires = [
 ]
 
 dev_requires = [
-    'pypitools',  # for upload and registration
-    'pydmt',  # for building
-    'pyclassifiers',  # for specifying classifications
-    'pymakehelper',  # for the makefile
+    'pypitools',
+    'pydmt',
+    'pyclassifiers',
+    'pymakehelper',
 ]
 
 python_requires = ">=3.6"
 
 extras_require = {
-    # ':python_version == "2.7"': ['futures'],  # for python2.7 backport of concurrent.futures
 }
