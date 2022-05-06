16666875d074cf93965c969c7564de5cebffc261
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:11:13 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,24 +2,18 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = []
-setup_requires = []
-run_requires = []
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'flake8',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
 ]
-install_requires = []
 dev_requires = [
-    'pypitools',
-    'pydmt',
-    'pyclassifiers',
-    'pymakehelper',
+    "pypitools",
+    "pydmt",
+    "pyclassifiers",
+    "pymakehelper",
 ]
-
-extras_require = {}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
