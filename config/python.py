2a8b5aaa52b5b8fd3563fd3f9e2d6b9258db2025
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 14:26:47 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,19 +2,20 @@ import config.project
 
 package_name = config.project.project_name
 
-test_requires = [
-    "pylint",
-    "pytest",
-    "pytest-cov",
-    "flake8",
-]
 dev_requires = [
     "pypitools",
     "pydmt",
     "pyclassifiers",
     "pymakehelper",
 ]
+test_requires = [
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+]
 
-python_requires = ">=3.9"
-test_os = ["ubuntu-20.04"]
-test_python = ["3.9"]
+python_requires = ">=3.10"
+
+test_os = ["ubuntu-22.04"]
+test_python = ["3.10"]
