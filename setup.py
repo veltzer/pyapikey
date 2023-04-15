e92956cdad77767791e264224fb3eea9bfbab112
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-15 14:10:39 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pyapikey",
-    version="0.0.2",
+    version="0.0.3",
     packages=[
         "pyapikey",
     ],
