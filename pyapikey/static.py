f175eb5403420d30c8faf06f1d86c5ed811743cd
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-15 14:51:48 +0300

    new version

--- pyapikey/static.py
+++ pyapikey/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.5"
+VERSION_STR = "0.0.6"
 DESCRIPTION = "access api keys from code"
 APP_NAME = "pyapikey"
 LOGGER_NAME = "pyapikey"
