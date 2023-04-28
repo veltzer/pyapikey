74479297195bbaf1c2f238e869c39a151ee815f2
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-28 07:15:53 +0300

    sphinx work

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('..'))
-
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.viewcode']
@@ -13,5 +7,3 @@ project_copyright = '2020, Mark Veltzer'
 author = 'Mark Veltzer'
 version = '0.0.1'
 release = '0.0.1'
-
-html_show_sphinx = False
