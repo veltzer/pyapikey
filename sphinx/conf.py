916fa4bca595667fc9f753518c4fc008e37a1795
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-28 07:01:02 +0300

    removed sphinx extra stuff

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -99,7 +99,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+# html_static_path = ['static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -170,5 +170,4 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-
+html_show_sphinx = False
