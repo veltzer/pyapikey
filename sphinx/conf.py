fc2178be39b78f6d4418be377ac04efe74f74e28
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-28 23:33:14 +0300

    no commit message given

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -2,8 +2,18 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.viewcode']
 
-project = 'pyapikey'
+
+import config.project
+project = config.project.name
 project_copyright = '2020, Mark Veltzer'
 author = 'Mark Veltzer'
-version = '0.0.1'
-release = '0.0.1'
+import config.version
+version = ".".join(str(x) for x in config.version.tup)
+release = ".".join(str(x) for x in config.version.tup)
+
+html_theme_options = {
+        "show_powered_by": "false",
+}
+rst_epilog = f'''
+.. |project| replace:: {project}
+'''
