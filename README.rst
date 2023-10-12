404910699fb8f3ff1febc804da918cbe80023c4c
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-12 19:04:18 +0300

    new version

--- README.rst
+++ README.rst
@@ -12,6 +12,6 @@ project website: https://veltzer.github.io/pyapikey
 
 author: Mark Veltzer
 
-version: 0.0.9
+version: 0.0.10
 
 	Mark Veltzer <mark.veltzer@gmail.com>, Copyright © 2020, 2021, 2022, 2023
--- pyapikey/static.py
+++ pyapikey/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.9"
+VERSION_STR = "0.0.10"
 DESCRIPTION = "access api keys from code"
 APP_NAME = "pyapikey"
 LOGGER_NAME = "pyapikey"
